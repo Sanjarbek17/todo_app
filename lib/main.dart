788dbfbfc048ee4abf6b7372c7030d6748c8c158
Sanjar17 <sanjarsaidov173@gmail.com>
2023-05-05 12:00:37 +0500

    api connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
 import 'providers/main_provider.dart';
 import 'screens/add_screen.dart';
 
-void main() {
+void main() async {
+  // load .env file
+  await dotenv.load(fileName: '.env');
   runApp(
     MultiProvider(
         providers: [
@@ -30,55 +33,64 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     // get list provider
     final listProvider = Provider.of<ListProvider>(context);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Todo List'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.add),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const AddPage(),
-                ),
+    // get data from api
+    listProvider.getData();
+    return FutureBuilder(
+        future: listProvider.getData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              appBar: AppBar(
+                centerTitle: true,
+                title: const Text('Todo List'),
+                actions: [
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const AddPage(),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.clear),
+                    onPressed: () => listProvider.clearList(),
+                  ),
+                ],
               ),
-            ),
-            IconButton(
-              icon: const Icon(Icons.clear),
-              onPressed: () => listProvider.clearList(),
-            ),
-          ],
-        ),
-        body: Center(
-          child: Container(
-            margin: const EdgeInsets.all(10),
-            width: 600,
-            child: ListView.builder(
-              // separatorBuilder: (context, index) => const Divider(),
-              itemBuilder: (context, index) => Container(
-                decoration: const BoxDecoration(border: Border(top: BorderSide(), bottom: BorderSide())),
-                child: ListTile(
-                  // give style to list tile
-                  // shape: RoundedRectangleBorder(
-                  //   side: const BorderSide(width: 2, color: Colors.amberAccent),
-                  //   borderRadius: BorderRadius.circular(10),
-                  // ),
-                  title: Text(listProvider.list[index]['title']),
-                  subtitle: Text(listProvider.list[index]['description']),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () => listProvider.removeFromList(index),
+              body: Center(
+                child: Container(
+                  margin: const EdgeInsets.all(10),
+                  width: 600,
+                  child: ListView.builder(
+                    // separatorBuilder: (context, index) => const Divider(),
+                    itemBuilder: (context, index) => Container(
+                      decoration: const BoxDecoration(border: Border(top: BorderSide())),
+                      child: ListTile(
+                        // give style to list tile
+                        // shape: RoundedRectangleBorder(
+                        //   side: const BorderSide(width: 2, color: Colors.amberAccent),
+                        //   borderRadius: BorderRadius.circular(10),
+                        // ),
+                        title: Text(listProvider.list[index]['name']),
+                        subtitle: Text(listProvider.list[index]['description']),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () => listProvider.removeFromList(index),
+                        ),
+                      ),
+                    ),
+                    itemCount: listProvider.list.length,
                   ),
                 ),
               ),
-              itemCount: listProvider.list.length,
             ),
-          ),
-        ),
-      ),
-    );
+          );
+        });
   }
 }
--- lib/providers/main_provider.dart
+++ lib/providers/main_provider.dart
@@ -1,15 +1,45 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import http package
+import 'package:http/http.dart' as http;
 
 class ListProvider extends ChangeNotifier {
+  // final String uri = 'http://localhost:3000';
+  final String uri = dotenv.env['API_URL'] ?? 'http://localhost:3000';
+
   final List<Map> _list = [
     {'title': 'asdf', 'description': 'description', 'status': false}
   ];
 
   List<Map> get list => _list;
 
-  void addToList(String name, String description, {bool status = false}) {
-    _list.add({'title': name, 'description': description, 'status': status});
+  void addToList(String name, String description, {bool status = false}) async {
+    Map data = {'name': name, 'description': description, 'status': status};
+    _list.add(data);
+    //request to api
+    final response = await http.post(
+      Uri.parse('$uri/api/create-task/'),
+      body: jsonEncode(data),
+      headers: {'Content-Type': 'application/json'},
+    );
+    print(response.body);
+    print(data);
     notifyListeners();
+  }
+
+  // get data from api
+  Future<void> getData() async {
+    final response = await http.get(Uri.parse('$uri/api/tasks/'));
+    if (response.statusCode == 200) {
+      _list.clear();
+      print(response.body);
+      Map<String, dynamic> data = jsonDecode(response.body);
+      for (var item in data['result']!) {
+        _list.add(item);
+      }
+    }
   }
 
   void removeFromList(int id) {
